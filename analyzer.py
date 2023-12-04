a96a3c20833e14e8cbb5cb708d76e3b56320f62f
a1exanderklein <agklein04@outlook.com>
2023-12-04 13:37:22 -0500

    Added Class

---
+++ analyzer.py
@@ -0,0 +1,106 @@
+import csv
+
+class ChessAnalyzer:
+    def __init__(self, selectedModes=None):
+        self.csvFile = 'sep.csv'
+        self.selectedModes = selectedModes if selectedModes is not None else []
+        self.mode = ""
+        self.openingData = []
+        self.winRatios = []
+
+    def printer(self):
+        for opening_ratio in self.openingData:
+            print(f"Opening: {opening_ratio[0]}, Success Ratio: {opening_ratio[1]}")
+
+    def openingAnalyzer(self, csvFile):
+        with open(csvFile, newline='') as csvfile:
+            #creates csv manager for traversing rows
+            csvReader = csv.DictReader(csvfile)
+            #iterate through each row in the CSV
+            for row in csvReader:
+                gameMode = row['Event']
+                if any(mode in gameMode for mode in self.selectedModes):
+                    result = row['Result']
+                    openingName = row['Opening']
+                    #do not account for openings with the '?'
+                    if openingName != "?":
+                        #check if the opening is already in the list
+                        found = False
+                        for openingVector in self.openingData:
+                            if openingVector[0] == openingName:
+                                #increment occurrences count
+                                openingVector[1] += 1
+                                #check if game result is a win (1-0)
+                                if result == '1-0':
+                                    openingVector[2] += 1
+                                found = True
+                                break
+
+                        #if opening not in list, add with initial counts based on the result
+                        if not found:
+                            self.openingData.append([openingName, 1, 1 if result == '1-0' else 0])
+
+    def winRateAnalyzer(self):
+        for openingStats in self.openingData:
+            name, occurrences, wins = openingStats #what is in the lists in the opening data list
+            if occurrences > 25: #only if it occurs over 25 to eliminate noise of 100% success rates in few games
+                successRatio = wins / occurrences if occurrences > 0 else 0
+                self.winRatios.append([name, successRatio])
+
+    #Referenced from DSA Module 6 - Sorting by Amanpreet Kapoor
+    #Time - Worst O(N^2)
+    #Space - O(log n) recursion stack
+    def quickSort(self, arr, low, high):
+        #check if sortable
+        if low < high:
+            pivot = self.partition(arr, low, high) #find pivot
+            self.quickSort(arr, low, pivot - 1) #call on left
+            self.quickSort(arr, pivot + 1, high) #call on right
+
+    def partition(self, arr, low, high):
+        pivot = arr[low][1] #choose first element of array for pivot
+        up = low
+        down = high
+
+        while up < down: #continue til they pass eachother
+            while up < high and arr[up][1] <= pivot: #while up is not past down & up is not greater than the pivot value
+                up += 1 #go right
+            while arr[down][1] > pivot: #while down is not less than pivot value
+                down -= 1 #go left
+            if up < down: #if up still < down, swap
+                arr[up], arr[down] = arr[down], arr[up]
+
+        arr[low], arr[down] = arr[down], arr[low] #swap pivot and down
+        return down #return pivot index after partition
+
+    #Time - Worst O(n log n)
+    #Space - O(n)
+    def mergeSort(self, arr):
+        if len(arr) > 1:
+            middle = len(arr)//2
+            left = arr[0:middle]
+            right = arr[middle:len(arr)]
+            self.mergeSort(left)
+            self.mergeSort(right)
+            l = 0
+            r = 0
+            i = 0
+            while l < len(left) and r < len(right):
+                if left[l][1] > right[r][1]:
+                    arr[i] = right[r]
+                    r = r + 1
+                    i = i + 1
+                else:
+                    arr[i] = left[l]
+                    l = l + 1
+                    i = i + 1
+            if l < len(left):
+                while l < len(left):
+                    arr[i] = left[l]
+                    i = i + 1
+                    l = l + 1
+            if r < len(right):
+                while r < len(right):
+                    arr[i] = right[r]
+                    i = i + 1
+                    r = r + 1
--- meta.py
+++ meta.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 import time
+from analyzer import ChessAnalyzer
 
-csvFile = 'sep.csv'
 # chess_dataframe = pd.read_csv(csvFile, delimiter=",", header= 0)
 # print(chess_dataframe.head)
 
@@ -35,107 +35,7 @@ while inputMode == '0':
     else:
         print("Invalid choice. Please enter a valid number.")
 
-
-openingData = [] #list of pairs (Opening, # of Occurrences, # of Wins)
-def openingAnalyzer(csvFile, openingDataArray):
-    with open(csvFile, newline='') as csvfile:
-        #creates csv manager for traversing rows
-        csvReader = csv.DictReader(csvfile)
-        #iterate through each row in the CSV
-        for row in csvReader:
-            gameMode = row['Event']
-            if any(mode in gameMode for mode in selectedModes):
-                result = row['Result']
-                openingName = row['Opening']
-                #do not account for openings with the '?'
-                if openingName != "?":
-                    #check if the opening is already in the list
-                    found = False
-                    for openingVector in openingDataArray:
-                        if openingVector[0] == openingName:
-                            #increment occurrences count
-                            openingVector[1] += 1
-                            #check if game result is a win (1-0)
-                            if result == '1-0':
-                                openingVector[2] += 1
-                            found = True
-                            break
-
-                    #if opening not in list, add with initial counts based on the result
-                    if not found:
-                        openingDataArray.append([openingName, 1, 1 if result == '1-0' else 0])
-
-#create separate list for win rates
-winRatios = []
-def winRateAnalyzer(winRatioArray):
-    for openingStats in openingData:
-        name, occurrences, wins = openingStats #what is in the lists in the opening data list
-        if occurrences > 25: #only if it occurs over 25 to eliminate noise of 100% success rates in few games
-            successRatio = wins / occurrences if occurrences > 0 else 0
-            winRatioArray.append([name, successRatio])
-
-def printer(arr):
-    for opening_ratio in arr:
-        print(f"Opening: {opening_ratio[0]}, Success Ratio: {opening_ratio[1]}")
-
-#Referenced from DSA Module 6 - Sorting by Amanpreet Kapoor
-#Time - Worst O(N^2)
-#Space - O(log n) recursion stack
-def quickSort(arr, low, high):
-    #check if sortable
-    if low < high:
-        pivot = partition(arr, low, high) #find pivot
-        quickSort(arr, low, pivot - 1) #call on left
-        quickSort(arr, pivot + 1, high) #call on right
-
-def partition(arr, low, high):
-    pivot = arr[low][1] #choose first element of array for pivot
-    up = low
-    down = high
-
-    while up < down: #continue til they pass eachother
-        while up < high and arr[up][1] <= pivot: #while up is not past down & up is not greater than the pivot value
-            up += 1 #go right
-        while arr[down][1] > pivot: #while down is not less than pivot value
-            down -= 1 #go left
-        if up < down: #if up still < down, swap
-            arr[up], arr[down] = arr[down], arr[up]
-
-    arr[low], arr[down] = arr[down], arr[low] #swap pivot and down
-    return down #return pivot index after partition
-
-#Time - Worst O(n log n)
-#Space - O(n)
-def mergeSort(arr):
-    if len(arr) > 1:
-        middle = len(arr)//2
-        left = arr[0:middle]
-        right = arr[middle:len(arr)]
-        mergeSort(left)
-        mergeSort(right)
-        l = 0
-        r = 0
-        i = 0
-        while l < len(left) and r < len(right):
-            if left[l][1] > right[r][1]:
-                arr[i] = right[r]
-                r = r + 1
-                i = i + 1
-            else:
-                arr[i] = left[l]
-                l = l + 1
-                i = i + 1
-        if l < len(left):
-            while l < len(left):
-                arr[i] = left[l]
-                i = i + 1
-                l = l + 1
-        if r < len(right):
-            while r < len(right):
-                arr[i] = right[r]
-                i = i + 1
-                r = r + 1
-
+analyzer = ChessAnalyzer(selectedModes=selectedModes)
 
 # Ask which sorting method they want to use
 print("Which sorting method would you like to use?\n(1) Merge Sort\n(2) Quick Sort")
@@ -145,18 +45,15 @@ while chosenSort == '0':
     chosenSort = input("Enter your choice: ")
     if chosenSort == '1':
         start = time.time()
-        mergeSort(winRatios)
+        analyzer.mergeSort(winRatios)
         end = time.time()
         print(f"Merge sort from {mode} completed in {end-start} seconds")
     elif chosenSort == '2':
         start = time.time()
-        quickSort(winRatios, 0, len(winRatios) - 1)
+        analyzer.quickSort(winRatios, 0, len(winRatios) - 1)
         end = time.time()
         print(f"Quick sort from {mode} completed in {end-start} seconds")
     else:
          print("Invalid choice. Please enter a valid number.")
 
-# quickSort(winRatios, 0, len(winRatios) - 1)
-
-# for opening_ratio in winRatios:
-#     print(f"Opening: {opening_ratio[0]}, Success Ratio: {opening_ratio[1]}")
+analyzer.printer()
